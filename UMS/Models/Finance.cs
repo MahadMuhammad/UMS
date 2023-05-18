797ebb401937f00bcbb30d7cd96c0c41a768ec5b
Muhammad Mahad <mahadtxt@gmail.com>
2023-05-18 10:59:45 +0500

    Added Finance class

--- UMS/Models/Finance.cs
+++ UMS/Models/Finance.cs
@@ -1,13 +1,21 @@
-﻿namespace UMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UMS.Models
 {
     public class Finance
     {
-        public int FinanceId { get; set; } // You can add a primary key for the Finance table if needed
+        //public int FinanceId { get; set; } // You can add a primary key for the Finance table if needed
+        [Required]
         public int RollNo { get; set; }
+
+        [Range(0, int.MaxValue)]
         public decimal TotalFee { get; set; }
+        [Range(0, int.MaxValue)]
         public decimal RemainingFee { get; set; }
 
         public Student Student { get; set; } // Navigation property
+
+
     }
 
 }
